c19aff607396fcec5d84f4d28beda9eaa0d43075
Lorenzo Cipriani <lorenzo1974@gmail.com>
2019-06-27 06:16:12 +0100

    Added descriptions of the steps into the script

--- setup.sh
+++ setup.sh
@@ -1,10 +1,43 @@
 #!/bin/bash
 
-sudo apt-get update && sudo apt-get install -y openvpn
+echo "
+###############################################################################
+###   Update the package list from the repository
+###############################################################################
+"
+sudo apt-get update
 
+echo "
+###############################################################################
+###   Install OpenVPN
+###############################################################################
+"
+sudo apt-get install -y openvpn
+
+echo "
+###############################################################################
+###   Remove old copies of VirtualHere server
+###############################################################################
+"
 if [ -f ./vhusbdarm ]; then
     rm ./vhusbdarm*
 fi
+if [ -f /usr/sbin/vhusbdarm ]; then
+    rm /usr/sbin/vhusbdarm*
+fi
+
+echo "
+###############################################################################
+###   Download VirtualHere server for ARM architecture
+###############################################################################
+"
 wget https://www.virtualhere.com/sites/default/files/usbserver/vhusbdarm
+
+echo "
+###############################################################################
+###   Install VirtualHere server
+###############################################################################
+"
 chmod +x ./vhusbdarm
-cp ./vhusbdarm /usr/sbin
+mv ./vhusbdarm /usr/sbin
+
